103d5e0c72b9006d373162b60749373c2c0643c4
Mohaiminul Islam <mohaiminul.sirat@gmail.com>
2022-04-20 23:38:05 +0200

    started designing menu items

--- src/components/body/MenuItem.js
+++ src/components/body/MenuItem.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
 const MenuItem = props => {
     console.log(props);
     return (
         <div>
-
-        </div>
+            <div>
+                <Card inverse>
+                    <CardImg
+                        alt={props.dish.name}
+                        src={props.dish.image}
+                        width="100%"
+                        style={{ opacity: "0.7" }}
+                    />
+                    <CardImgOverlay>
+                        <CardTitle tag="h4" style={{ color: 'black', fontFamily: 'Tahoma', WebkitTextStroke: '1px purple' }}>
+                            {props.dish.name}
+                        </CardTitle>
+                    </CardImgOverlay>
+                </Card>
+            </div>
+        </div >
     );
 }
 
